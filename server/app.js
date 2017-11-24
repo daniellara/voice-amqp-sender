56215009aaebd8f4b5a7d65f1f8aab2103fa3aae
DanielLara <daniel.lara.diezma@gmail.com>
2017-11-24 23:25:32 +0100

    Add amqp sender with the message

--- server/app.js
+++ server/app.js
@@ -1,16 +1,26 @@
+var amqp = require('amqplib');
 const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
 const io = require('socket.io').listen(server);
+const q = 'queue';
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('chat message', (msg) => {
-    console.log('Message: ', msg);
+amqp.connect('amqp://localhost')
+  .then((conn) => conn.createChannel())
+  .then((ch) =>  ch.assertQueue(q)
+    .then((ok) => {
+      io.on('connection', (socket) => {
+        socket.on('chat message', (msg) => {
+          console.log('Message: ', msg);
+          ch.sendToQueue(q, new Buffer(msg));
+        });
+      });
+    }))
+  .catch((err) => {
+    console.log(err);
   });
-});
 
 app.use(express.static(path.join(__dirname, '../public')));
 
