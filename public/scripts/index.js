7652043fbb2a73c5c18298541760a249b872c184
DanielLara <daniel.lara.diezma@gmail.com>
2017-11-24 22:59:31 +0100

    Add SpeechRecognition

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,16 @@
-console.log('Script loaded');
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'es-ES';
+
+recognition.start();
+
+recognition.onresult = (e) => {
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  console.log('Texto: ', text);
+};
+
+recognition.onend = () => {
+  recognition.start();
+};
